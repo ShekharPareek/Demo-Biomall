341ea40f0db2381d1876051c1ca381516b361e40
Shekhar <108127963+ShekharPareek@users.noreply.github.com>
2025-09-06 03:08:08 +0000

    code push

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+'use client'
+import Header from './components/header';
+import HomePage from './components/AIAssistant';
+import Footer from './components/footer';
+
+export default function Home() {
+
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  );
+}
